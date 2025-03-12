b8313bdabfdb67f69e5b08434a4d676abf8911b4
Jose F Rivas C <josefelixrc7@gmail.com>
2025-03-12 16:22:16 -0400

    feat: Add https

--- app.js
+++ app.js
@@ -1,19 +1,30 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
+import https from 'https';
 
 import salesStatesHandler from './handlers/salesStatesHandler.js';
 
 dotenv.config();
 
+// Lee los archivos de certificado y clave
+const privateKey = fs.readFileSync(process.env.KEY, 'utf8');
+const certificate = fs.readFileSync(process.env.CERT, 'utf8');
+
+const credentials = {
+  key: privateKey,
+  cert: certificate,
+};
+
 const app = express();
+const httpsServer = https.createServer(credentials, app);
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Rutas
 app.use(salesStatesHandler);
 
-app.listen(process.env.PORT, () =>
+httpsServer.listen(process.env.PORT, () =>
 {
     console.log(`oceanocc-panel ${process.env.PORT}`);
 });
