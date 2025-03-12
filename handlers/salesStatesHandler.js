71cd2d6e846d1ab74c3d100cfa56324695c68f56
Jose F Rivas C <josefelixrc7@gmail.com>
2025-03-12 16:27:30 -0400

    feat: Fix sales states handler

--- handlers/salesStatesHandler.js
+++ handlers/salesStatesHandler.js
@@ -10,35 +10,28 @@ handler.get('/salesStates', async (req, res) =>
 
     const salesStates = async (usuario, clave, fecha_inicio, fecha_fin) =>
     {
-        if (!fecha_inicio || !fecha_fin)
-        {
-            res.status(500).json({ message: 'La fecha de inicio o fin no pueden estar vacías' });
-        }
-        else
-        {
-            const [rows] = await pool.query
-            (
-                `
-                    SELECT s.*
-                    FROM (
-                        SELECT
-                            u.usuario AS 'usuario'
-                            ,v.dn AS 'dn'
-                            ,ev.status AS 'status'
-                            ,v.fecha_venta AS 'fecha_venta'
-                            ,ev.fecha_activacion AS 'fecha_activacion'
-                            ,ev.fecha_alta AS 'fecha_alta'
-                            ,ev.fecha_actualizacion AS 'fecha_actualizacion'
-                        FROM ventas v
-                        JOIN usuarios u ON u.id = v.id_usuario AND u.usuario = ? AND u.clave = ?
-                        LEFT JOIN estado_de_ventas ev ON ev.dn = v.dn
-                    ) s
-                    WHERE s.fecha_venta BETWEEN ? AND ?
-                `
-                ,[usuario, clave, fecha_inicio, fecha_fin]
-            );
-            return rows;
-        }
+        const [rows] = await pool.query
+        (
+            `
+                SELECT s.*
+                FROM (
+                    SELECT
+                        u.usuario AS 'usuario'
+                        ,v.dn AS 'dn'
+                        ,ev.status AS 'status'
+                        ,v.fecha_venta AS 'fecha_venta'
+                        ,ev.fecha_activacion AS 'fecha_activacion'
+                        ,ev.fecha_alta AS 'fecha_alta'
+                        ,ev.fecha_actualizacion AS 'fecha_actualizacion'
+                    FROM ventas v
+                    JOIN usuarios u ON u.id = v.id_usuario AND u.usuario = ? AND u.clave = ?
+                    LEFT JOIN estado_de_ventas ev ON ev.dn = v.dn
+                ) s
+                WHERE s.fecha_venta BETWEEN ? AND ?
+            `
+            ,[usuario, clave, fecha_inicio, fecha_fin]
+        );
+        return rows;
     }
     const results = await salesStates(user, password, from, to);
 
