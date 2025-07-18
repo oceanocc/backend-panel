07abac3268853b4a3ab780d6b116c5e2a5d584ff
Jose F Rivas C <josefelixrc7@gmail.com>
2025-07-18 00:06:30 -0400

    feat: Improve reports handler

--- handlers/reportsHandler.js
+++ handlers/reportsHandler.js
@@ -4,16 +4,33 @@ import { isAuthenticated } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
-router.get('/api/reports/agentsIndicators', isAuthenticated, async (req, res) =>
+// Report1: Agents time
+router.get('/api/reports/1', isAuthenticated, async (req, res) =>
 {
-    const { from, to } = req.body;
+    const { from, to } = req.query;
 
     try
     {
-        const [rows] = await pool.query(
-            'SELECT COUNT(1) FROM asterisk.vicidial_log',
-            [req.session.space, id]
-        );
+        const [rows] = await pool.query(`
+            SELECT
+                s.user AS 'cedula'
+                ,vu.full_name AS 'nombre_completo'
+                ,IFNULL(ROUND(SUM(s.pause_sec + s.wait_sec + s.talk_sec + s.dispo_sec)/3600, 2),'0') AS 'horas'
+                ,ROUND((SUM(s.talk_sec) - SUM(s.dead_sec))/3600, 2) AS 'acd'
+                ,ROUND(SUM(s.wait_sec)/3600, 2) AS 'wait'
+                ,ROUND(SUM(s.dispo_sec)/3600, 2) AS 'acw'
+                ,ROUND(SUM(s.dead_sec)/3600, 2) AS 'dead'
+                ,ROUND(SUM(s.pause_sec)/3600, 2) AS 'pauseAux'
+                ,ROUND(SUM(IF(s.sub_status='BREAK',s.pause_sec,0))/3600, 2) AS 'break'
+                ,ROUND(SUM(IF(s.sub_status='BANO',s.pause_sec,0))/3600, 2) AS 'bano'
+                ,ROUND(SUM(IF(s.sub_status='FDBCK',s.pause_sec,0))/3600, 2) AS 'fdbk'
+                ,ROUND(SUM(IF(s.sub_status='MANUAL',s.pause_sec,0))/3600, 2) AS 'manual'
+                ,ROUND(SUM(IF(s.sub_status='LOGIN',s.pause_sec,0))/3600, 2) AS 'login'
+            FROM asterisk_test.vicidial_agent_log s
+            JOIN asterisk_test.vicidial_users vu ON vu.user = s.user
+            WHERE s.event_time BETWEEN ? AND ? + INTERVAL 1 DAY
+            GROUP BY s.user
+        `, [from, to]);
         res.json({ data: rows });
     }
     catch (error)
